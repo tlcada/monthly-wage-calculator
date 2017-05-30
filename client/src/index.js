e3ce13bcd79e32176b94ca682e84b58eaa293290
Petteri Aho <aho.petteri@gmail.com>
2017-05-30 20:58:40 +0300

    Code refactoring

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { install } from 'offline-plugin/runtime'
+import * as OfflinePluginRuntime from 'offline-plugin/runtime'
 
 import './base.scss'
 import App from './components/App/App'
@@ -10,5 +10,5 @@ ReactDOM.render(
 )
 
 if (process.env.NODE_ENV === 'production') {
-  install()
+  OfflinePluginRuntime.install()
 }
